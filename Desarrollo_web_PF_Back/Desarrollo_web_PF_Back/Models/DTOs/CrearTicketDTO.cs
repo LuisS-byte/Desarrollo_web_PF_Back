555bbc16de08552f8e4a34d614df1e44e1a81527
ErnestoT04 <eernestoj21@gmail.com>
2025-06-01 11:37:04 -0600

    Añadido del DTO para tickets

---
+++ Desarrollo_web_PF_Back/Desarrollo_web_PF_Back/Models/DTOs/CrearTicketDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Desarrollo_web_PF_Back.Models.DTOs
+{
+    public class CrearTicketDTO
+    {
+
+        public int IdServicio { get; set; }
+        public int IdPrioridad { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public IFormFile? Archivo { get; set; } // opcional
+    }
+}
